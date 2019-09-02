d5daf96d21a600a27e253211395d740d2208d9ef
wybosys <wybosys@gmail.com>
2019-09-02 21:03:05 +0800

    prj

--- tools/reinit-git-projects-tree.py
+++ tools/reinit-git-projects-tree.py
@@ -5,8 +5,19 @@ import os, git, sys, json, re
 tree=sys.argv[1]
 out=sys.argv[2]
 
-def process(node, dir):
-    pass
+def process(nodes, dir):
+    for node in nodes:
+        path = out + node['path']
+        print(path)
+        if os.path.isdir(path):
+            now = os.getcwd()
+            os.chdir(path)
+            os.system('git pull')
+            os.chdir(now)
+        else:            
+            cur = node['remotes'][0]
+            url = cur['urls'][0]
+            os.system('git clone ' + url + ' ' + path)
 
-nodes=json.loads(''.join(open('tree').readlines()))
+nodes=json.loads(''.join(open(tree).readlines()))
 process(nodes, out)
